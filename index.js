168b0c603b7853f0d277eed4c256ec50e52807de
Your Name <you@example.com>
2023-01-30 17:44:23 +0600

    get employes data

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const express = require('express') ; 
-const cors = require('cros') ; 
-const app = express() ; 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config() ; 
+const app = express();
 
-const port = process.env.PORT || 5000  ; 
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=> {
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>
+{
     res.send({
-        message : 'This server Group aisa connect'
+        message: 'This server Group aisa connect'
     })
 })
 
-app.listen(port , ()=> {
-    console.log(`This server Group aisa is connected port is ${port}`)
+const user = process.env.user_name ; 
+const password = process.env.user_password ; 
+
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.5rnuhbi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    const AdminCollection = client.db('Gropu-Asia').collection('Admin');
+    const EmployesCollection = client.db('Gropu-Asia').collection('CreateEmploye');
+    try{
+        app.get('/admin',async (req, res)=> {
+            const query = {} ;
+            const result = await AdminCollection.find(query).toArray();
+            res.send(result)  ; 
+        })
+        app.get('/crateEmployes', async(req, res)=> {
+            const query = {} ; 
+            const  result = await EmployesCollection.find(query).toArray();
+            res.send(result) ; 
+        })
+        app.post('/createEmployes', async (req, res)=>{
+            const query = req.body ; 
+            const result = await EmployesCollection.insertOne(query)
+            res.send(result) 
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch((error)=> console.log(error))
+
+
+app.listen(port, () =>
+{
+    console.log(`This server Group aisa is connected port is ${ port }`)
 })
